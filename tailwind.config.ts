b8d7375f3424236f3a24af7fb3988ef20a3d3a7e
Saikat Samanta <95569573+psykat1116@users.noreply.github.com>
2023-09-21 19:39:12 +0530

    Add Descriptive Event Page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,19 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      backgroundImage: {
+        "event-back-image":
+          "linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url('/Images/events-bg.jpg')",
+        "rule-bg": "url('/Images/Untitled-2.png')",
+      },
+    },
+    fontFamily: {
+      harry: ["Harry P", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+    },
+  },
   plugins: [],
 };
 export default config;
