a8076e57577b3fd761168c07b08036f1565e3b48
Animesh <2025.cse18.animeshbag@gmail.com>
2023-09-23 02:53:26 +0530

    Signup, Login backend and schemas added

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,54 @@
+import connect from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
+
+connect();
+
+export async function POST(request: NextRequest){
+    try {
+
+        const reqBody = await request.json();
+        const {name, year, dept, roll, phone, email, password, payReceipt} = reqBody;
+
+        const user = await User.findOne({payReceipt, isVerified : true});
+
+        if(user){
+            return NextResponse.json({error: "User already exists"}, {status: 400});
+        }
+
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
+        const newUser = new User({
+            name,
+            year, 
+            dept, 
+            roll, 
+            phone, 
+            email, 
+            password: hashedPassword, 
+            payReceipt
+        });
+
+        const savedUser = await newUser.save();
+
+        //send verification email
+
+        await sendEmail({email, emailType: "VERIFY", userId: savedUser._id});
+
+        return NextResponse.json({
+            message: "User created successfully",
+            success: true,
+            savedUser
+        })
+        
+        
+
+
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+
+    }
+}
---
+++ src/models/userModel.ts
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a Name"]
+    },
+    year: {
+        type: Number,
+        required: [true, "Please provide a year"],
+        enum: [1, 2]
+    },
+    dept: {
+        type: String,
+        required: [true, "Please provide a Department"],
+        enum: ["CSE", "IT", "ECE", "EE", "ME", "CE"]
+    },
+    roll: {
+        type: String,
+        required: [true, "Please provide a Roll No"],
+        unique: true
+    },
+    phone: {
+        type: String,
+        required: [true, "Please provide a Mobile No"],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide a email"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    payReceipt: {
+        type: String,
+        unique: true
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+})
+
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
