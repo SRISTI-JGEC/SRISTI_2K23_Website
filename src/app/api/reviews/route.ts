ec9a61645d30857b4855457a50cf99de9d9e0d9d
sarthakroy107 <sarthakroy107@gmail.com>
2023-09-24 15:59:21 +0530

    formatted documents for better readibility and added comments

--- src/app/api/reviews/route.ts
+++ src/app/api/reviews/route.ts
@@ -12,46 +12,49 @@ export interface formattedReviews {
 
 connect();
 
-export async function POST(request: NextRequest){
+export async function POST(request: NextRequest) {
     try {
         const reqBody = await request.json();
-        const {name, email, review, department, year, college} = reqBody;
-        console.log(name, email, review, department, year, college);
+        const { name, email, review, department, year, college } = reqBody;
 
-        const reviewAlreadyExists = await Review.findOne({email});
-        if(reviewAlreadyExists){
-            return NextResponse.json({error: "Review already exists"}, {status: 400});
+        //cheack if the review already exists
+        const reviewAlreadyExists = await Review.findOne({ email });
+        if (reviewAlreadyExists) {
+            return NextResponse.json({ error: "Review already exists" }, { status: 400 });
         }
-        const newReview = await Review.create({name, email, review, department, year, college});
-        console.log(newReview);
+        //create a new review
+        const newReview = await Review.create({ name, email, review, department, year, college });
 
-        return NextResponse.json({message: "Review created successg=fully"}, {status: 201});
+        return NextResponse.json({ message: "Review created successg=fully" }, { status: 201 });
+
     } catch (error: any) {
         console.log(error.message);
-        return NextResponse.json({error: error.message}, {status: 500});
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 };
 
-export async function GET(request: NextRequest){
+export async function GET(request: NextRequest) {
     try {
+        //get all reviews
         const reviews = await Review.find({});
-        console.log(reviews);
+
+        //format the reviews
         let formated_reviews_object: formattedReviews[] = []
-        for(const review of reviews) {
+        for (const review of reviews) {
             const object = {
-                name:        review?.name,
-                year:        review?.year,
-                review:      review?.review,
-                college:     review?.college === undefined ? "" : review?.college,
-                department:  review?.department
+                name: review?.name,
+                year: review?.year,
+                review: review?.review,
+                college: review?.college === undefined ? "" : review?.college,
+                department: review?.department
             }
             formated_reviews_object.push(object);
         }
 
-        return NextResponse.json({formated_reviews_object}, {status: 200});
+        return NextResponse.json({ formated_reviews_object }, { status: 200 });
 
     } catch (error: any) {
-        console.log(error.message);
-        return NextResponse.json({error: error.message}, {status: 500});
+
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 };
--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -1,18 +1,18 @@
 import { formattedReviews } from '@/app/api/reviews/route'
 import React from 'react'
 
-const ReviewCard = ({review}: {review: formattedReviews}) => {
+const ReviewCard = ({ review }: { review: formattedReviews }) => {
   return (
-    <div style={{backgroundImage: "url('/Images/Untitled-2.png')", backgroundSize: 'contain'}  }
-     className='w-[18.5rem] h-[15.6rem] lg:w-[32.5rem] lg:h-[27.5rem] ml-10 lg:ml-12 text-white p-5 lg:p-16 text-sm lg:text-xl font-medium'>
-        <i>{review.review}</i>
-        <p className='w-full text-right mt-5'>~{review.name}</p>
-        <p className='w-full text-right mt-1 text-sm lg:text-lg text-slate-50/75'>{review.department} , &nbsp;
+    <div style={{ backgroundImage: "url('/Images/Untitled-2.png')", backgroundSize: 'contain' }}
+      className='w-[18.5rem] h-[15.6rem] lg:w-[32.5rem] lg:h-[27.5rem] ml-10 lg:ml-12 text-white p-5 lg:p-16 text-sm lg:text-xl font-medium'>
+      <i>{review.review}</i>
+      <p className='w-full text-right mt-5'>~{review.name}</p>
+      <p className='w-full text-right mt-1 text-sm lg:text-lg text-slate-50/75'>{review.department} , &nbsp;
         {
           review.year === "1" ? review.year + "st Year" : review.year === "2" ? review.year + "nd Year" :
-          review.year === "3" ? review.year + "rd Year" : review.year + "th Year"
+            review.year === "3" ? review.year + "rd Year" : review.year + "th Year"
         }
-        </p>
+      </p>
     </div>
   )
 }
