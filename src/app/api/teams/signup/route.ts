e5e9593a08a334c7cdfffa3b314a99745f8c8299
Swadesh Pal <97722877+Swadeshit27@users.noreply.github.com>
2023-09-26 13:56:10 +0530

    Update route.ts

--- src/app/api/teams/signup/route.ts
+++ src/app/api/teams/signup/route.ts
@@ -5,16 +5,9 @@ import { sendEmail } from "@/helpers/mailer";
 
 connect();
 
-export async function POST(request: NextRequest) {
-  try {
-    const reqBody = await request.json();
-    const { teamName, eventName, leadEmail, members } = reqBody;
+export async function POST(request: NextRequest){
+    try {
 
-<<<<<<< HEAD
-    const team = await Team.findOne({ eventName });
-    const team1 = await Team.findOne({ leadEmail });
-    const team2 = await Team.findOne({ eventName, "members.email": leadEmail });
-=======
         const reqBody = await request.json();
         const {teamName, eventName, leadEmail, members} = reqBody;
 
@@ -58,58 +51,6 @@ export async function POST(request: NextRequest) {
 
     } catch (error: any) {
         return NextResponse.json({error: error.message}, {status: 500})
->>>>>>> 70607b216ef279e8679cba1a5bc76eb5efbc8e67
 
-    if (team && (team1 || team2)) {
-      return NextResponse.json(
-        {
-          error:
-            "Team already exists or Lead is already been registered in an other teeam",
-        },
-        { status: 400 }
-      );
     }
-
-    for (let i = 0; i < members.length; i++) {
-      let find = await Team.findOne({
-        eventName,
-        "members.email": members[i].email,
-      });
-      let find2 = await Team.findOne({
-        eventName,
-        leadEmail: members[i].email,
-      });
-      if (find || find2)
-        return NextResponse.json(
-          { error: `${members[i].email} is already registered` },
-          { status: 400 }
-        );
-    }
-
-    const newTeam = new Team({
-      teamName,
-      eventName,
-      leadEmail,
-      members,
-    });
-
-    const savedTeam = await newTeam.save();
-
-    //send verification email
-    for (let i = 0; i < members.length; i++)
-      await sendEmail({
-        email: members[i].email,
-        emailType: "INVITATION",
-        teamId: savedTeam._id,
-        userId: savedTeam.members[i]._id,
-      });
-
-    return NextResponse.json({
-      message: "Team created successfully",
-      success: true,
-      savedTeam,
-    });
-  } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
 }
