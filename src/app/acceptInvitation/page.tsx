d055e00f77ff9f5a32b1ccafd75fd9579dd38e20
Swadesh Pal <swadeshpal2002@gmail.com>
2023-09-29 11:38:31 +0530

    update

--- src/app/acceptInvitation/page.tsx
+++ src/app/acceptInvitation/page.tsx
@@ -3,61 +3,54 @@
 import axios from "axios";
 import { getCookieParser } from "next/dist/server/api-utils";
 import Link from "next/link";
-import { NextRequest } from "next/server";
+
 import React, { useEffect, useState } from "react";
 
+export default function VerifyEmailPage() {
+  const [token, setToken] = useState("");
+  const [verified, setVerified] = useState(false);
+  const [error, setError] = useState(false);
 
-export default function VerifyEmailPage(request : NextRequest) {
-
-    const [token, setToken] = useState("");
-    const [verified, setVerified] = useState(false);
-    const [error, setError] = useState(false);
-
-    const verifyUserEmail = async () => {
-        try {
-            await axios.post('/api/users/acceptInvitation', {token})
-            setVerified(true);
-        } catch (error:any) {
-            setError(true);
-            console.log(error.reponse.data); 
-        }
-
+  const verifyUserEmail = async () => {
+    try {
+      await axios.post("/api/users/acceptInvitation", { token });
+      setVerified(true);
+    } catch (error: any) {
+      setError(true);
+      console.log(error.reponse.data);
     }
+  };
 
-    useEffect(() => {
-        const urlToken = window.location.search.split("=")[1];
-        console.log(urlToken);
-        setToken(urlToken || "");
-    }, []);
+  useEffect(() => {
+    const urlToken = window.location.search.split("=")[1];
+    console.log(urlToken);
+    setToken(urlToken || "");
+  }, []);
 
+  useEffect(() => {
+    if (token.length > 0) {
+      verifyUserEmail();
+    }
+  }, [token]);
 
-    useEffect(() => {
-        if(token.length > 0) {
-            verifyUserEmail();
-        }
-    }, [token]);
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl">Accepting Invitation</h1>
+      <h2 className="p-2 bg-orange-500 text-black">
+        {token ? `${token}` : "no token"}
+      </h2>
 
-    return(
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-
-            <h1 className="text-4xl">Accepting Invitation</h1>
-            <h2 className="p-2 bg-orange-500 text-black">{token ? `${token}` : "no token"}</h2>
-
-            {verified && (
-                <div>
-                    <h2 className="text-2xl">Invitation Accepted</h2>
-                    <Link href="/">
-                        Home Page
-                    </Link>
-                </div>
-            )}
-            {error && (
-                <div>
-                    <h2 className="text-2xl bg-red-500 text-black">Error</h2>
-                    
-                </div>
-            )}
+      {verified && (
+        <div>
+          <h2 className="text-2xl">Invitation Accepted</h2>
+          <Link href="/">Home Page</Link>
         </div>
-    )
-
+      )}
+      {error && (
+        <div>
+          <h2 className="text-2xl bg-red-500 text-black">Error</h2>
+        </div>
+      )}
+    </div>
+  );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,30 +1,105 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+ 
+import Image from "next/image";
 
 const Page = () => {
+ 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/users/profile");
+        console.log(response);
+         
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    const [user, setUser] = useState({});
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('/api/users/profile');
-                setUser(response);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-    
-        fetchData();
-    
-    }, []);
-
-    return (
-        <div>
-            user
+  return (
+    <>
+      <Image
+        alt="bg"
+        priority
+        src="/Images/43.harry-potter-following-the-darkness-4k-tp.jpg"
+        width={1000}
+        height={1000}
+        className="-z-10 fixed inset-0 w-full h-full object-cover"
+        draggable={false}
+      />
+      <div className="min-h-screen w-full flex justify-between items-start mt-20 font-griffy text-white ">
+        <div className="w-1/2 flex justify-center items-center py-10 px-4">
+          <div className="h-[500px] w-[400px] bg-[rgba(0,0,0,0.6)] backdrop-blur-md p-5 flex flex-col justify-between items-start">
+          
+            <div className="flex justify-between items-center w-full text-lg">
+              <h4>Name :</h4>
+              <p className="font-normal">Saikat Samanta</p>
+            </div>
+            <div className="flex justify-between items-center w-full text-lg">
+              <h4>Email :</h4>
+              <p className="font-normal">MyMail@gmail.com</p>
+            </div>
+            <div className="flex justify-between items-center w-full text-lg">
+              <h4>Mobile No :</h4>
+              <p className="font-normal">+91 2482348781</p>
+            </div>
+            <div className="flex justify-between items-center w-full text-lg">
+              <h4>Department :</h4>
+              <p className="font-normal">Information Technology</p>
+            </div>
+            <div className="flex justify-between items-center w-full text-lg">
+              <h4>Year :</h4>
+              <p className="font-normal">1st Year</p>
+            </div>
+            <div className="flex justify-between items-center w-full text-lg">
+              <h4>Roll No :</h4>
+              <p className="font-normal">21101106027</p>
+            </div>
+            <div className="flex justify-between items-center w-full text-lg">
+              <h4>Bill No :</h4>
+              <p className="font-normal">wri238ry2e28</p>
+            </div>
+          </div>
         </div>
-    );
+        <div className="w-1/2 py-10 px-4 flex justify-start items-center flex-col">
+          {/* <h1 className="text-4xl font-bold mb-5">My Events</h1> */}
+          <div className="min-h-[500px] w-[600px] bg-[rgba(0,0,0,0.6)] p-5 backdrop-blur-sm grid grid-cols-3">
+            <button className="card-box h-[80px] w-[160px] flex justify-center items-center text-xl cursor-pointer rounded-md border-white border-2">
+              Mesmarise
+            </button>
+            <button className="card-box h-[80px] w-[160px] flex justify-center items-center text-xl cursor-pointer rounded-md border-white border-2">
+              CSS Battle
+            </button>
+            <button className="card-box h-[80px] w-[160px] flex justify-center items-center text-xl cursor-pointer rounded-md border-white border-2">
+              Code Cronicles
+            </button>
+            <button className="card-box h-[80px] w-[160px] flex justify-center items-center text-xl cursor-pointer rounded-md border-white border-2">
+              Rocketary
+            </button>
+            <button className="card-box h-[80px] w-[160px] flex justify-center items-center text-xl cursor-pointer rounded-md border-white border-2">
+              Rocketary
+            </button>
+            <button className="card-box h-[80px] w-[160px] flex justify-center items-center text-xl cursor-pointer rounded-md border-white border-2">
+              Rocketary
+            </button>
+            <button className="card-box h-[80px] w-[160px] flex justify-center items-center text-xl cursor-pointer rounded-md border-white border-2">
+              Rocketary
+            </button>
+            <button className="card-box h-[80px] w-[160px] flex justify-center items-center text-xl cursor-pointer rounded-md border-white border-2">
+              Rocketary
+            </button>
+            <button className="card-box h-[80px] w-[160px] flex justify-center items-center text-xl cursor-pointer rounded-md border-white border-2">
+              Rocketary
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Page;
