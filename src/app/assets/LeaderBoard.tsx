ad9554d05bd3bb4ad21312b977a55225a55bfa6f
Swadesh Pal <swadeshpal2002@gmail.com>
2023-10-07 16:53:05 +0530

    update

---
+++ src/app/assets/LeaderBoard.tsx
@@ -0,0 +1,89 @@
+export type leaderBoardType = {
+  id: number;
+  category: string;
+  winners: string[];
+};
+
+export const leaderBoardList: leaderBoardType[] = [
+  {
+    id: 1,
+    category: "mazemarize",
+    winners: ["", "", ""],
+  },
+  {
+    id: 2,
+    category: "roboSoccer",
+    winners: ["", "", ""],
+  },
+  {
+    id: 3,
+    category: "sputnik",
+    winners: ["Pheonix", "Universe ", "Virus"],
+  },
+  {
+    id: 4,
+    category: "rotolare",
+    winners: ["Phoenix", "Slytherin Nurds", "Tandav"],
+  },
+  {
+    id: 5,
+    category: "bridgeTheGap",
+    winners: ["", "", ""],
+  },
+  {
+    id: 6,
+    category: "codeChronicles",
+    winners: ["", "", ""],
+  },
+  {
+    id: 7,
+    category: "cssBattle",
+    winners: ["", "", ""],
+  },
+  {
+    id: 8,
+    category: "captureTheFlag",
+    winners: ["", "", ""],
+  },
+  {
+    id: 9,
+    category: "sharkTankJolu",
+    winners: ["", "", ""],
+  },
+
+  {
+    id: 10,
+    category: "gmun",
+    winners: ["Sayan Ganguly", "Samridha Ghatak", "Ankit Ghosh"],
+  },
+  {
+    id: 11,
+    category: "intraRicerca",
+    winners: ["", "", ""],
+  },
+  {
+    id: 12,
+    category: "troubleshooter",
+    winners: ["", "", ""],
+  },
+  {
+    id: 13,
+    category: "techzibition",
+    winners: ["", "", ""],
+  },
+  {
+    id: 15,
+    category: "techQuiz",
+    winners: ["", "", ""],
+  },
+  {
+    id: 17,
+    category: "mathemania",
+    winners: ["", "", ""],
+  },
+  {
+    id: 18,
+    category: "codeHunt",
+    winners: ["", "", ""],
+  },
+];
--- src/app/events/[eventname]/[leaderboard]/page.tsx
+++ src/app/events/[eventname]/[leaderboard]/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
-import Image from "next/image";
+"use client";
 
+import Image from "next/image";
+import { useParams, useRouter } from "next/navigation";
+import { leaderBoardType } from "@/app/assets/LeaderBoard";
+import { leaderBoardList } from "@/app/assets/LeaderBoard";
+import { useEffect, useState } from "react";
 const LeaderBoard = () => {
+  const { leaderboard } = useParams();
+  const [list, setList] = useState<leaderBoardType>();
+  const findLeaderBoard = () => {
+    const data = leaderBoardList.find(
+      (items) => items.category === leaderboard
+    );
+    console.log(data);
+    setList(data);
+  };
+  useEffect(() => {
+    findLeaderBoard();
+  }, []);
   return (
     <>
       <div className="w-full min-h-screen  overflow-y-scroll bg-leaderboard_Image bg-no-repeat bg-cover">
@@ -18,108 +34,68 @@ const LeaderBoard = () => {
               className="w-[30rem] max-sm:w-[20rem]"
             />
           </div>
-          {/* <div className="w-full h-auto flex flex-col justify-center items-center">
-            <div className="w-[20rem] min-h-[10rem] mx-auto flex flex-col items-center">
-              <Image
-                src="/Images/1st.png"
-                alt=""
-                width={200}
-                height={200}
-                className="object-cover "
-              />
-              <div className="w-full min-h-24 rounded-lg bg-yellow-300 ">
-                <ul className="p-4">
-                  <li className="text-xl font-semibold text-center mb-2">
-                    Team <span className="capitalize">geronimo</span>
-                  </li>
-                  <li className="text-sm font-[500] capitalize ps-4">
-                    <span className="pe-3">1)</span>swadesh pal
-                  </li>
-                  <li className="text-sm font-[500] capitalize ps-4">
-                    <span className="pe-3">2)</span>swadesh pal
-                  </li>
-                  <li className="text-sm font-[500] capitalize ps-4">
-                    <span className="pe-3">3)</span>swadesh pal
-                  </li>
-                  <li className="text-sm font-[500] capitalize ps-4">
-                    <span className="pe-3">4)</span>swadesh pal
-                  </li>
-                  <li className="text-sm font-[500] capitalize ps-4">
-                    <span className="pe-3">5)</span>swadesh pal
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="flex justify-between max-lg:items-center w-full min-h-[18rem] px-8 max-lg:flex-col">
-              <div className="relative w-[18rem] h-full  flex flex-col items-center max-sm:mt-4 ">
+          {list?.winners[0] ? (
+            <div className="w-full h-auto flex flex-col justify-center items-center">
+              <div className="w-[20rem] min-h-[10rem] mx-auto flex flex-col items-center">
                 <Image
-                  src="/Images/2nd.png"
+                  src="/Images/1st.png"
                   alt=""
                   width={200}
                   height={200}
                   className="object-cover "
                 />
-                <div className="w-full min-h-24 rounded-lg bg-gray-300 ">
+                <div className="w-full min-h-24 rounded-lg bg-yellow-300 ">
                   <ul className="p-4">
                     <li className="text-xl font-semibold text-center mb-2">
-                      Team <span className="capitalize">geronimo</span>
-                    </li>
-                    <li className="text-sm font-[500] capitalize ps-4">
-                      <span className="pe-3">1)</span>swadesh pal
-                    </li>
-                    <li className="text-sm font-[500] capitalize ps-4">
-                      <span className="pe-3">2)</span>swadesh pal
-                    </li>
-                    <li className="text-sm font-[500] capitalize ps-4">
-                      <span className="pe-3">3)</span>swadesh pal
-                    </li>
-                    <li className="text-sm font-[500] capitalize ps-4">
-                      <span className="pe-3">4)</span>swadesh pal
-                    </li>
-                    <li className="text-sm font-[500] capitalize ps-4">
-                      <span className="pe-3">5)</span>swadesh pal
+                      Team <span className="capitalize">{list.winners[0]}</span>
                     </li>
                   </ul>
                 </div>
               </div>
-              <div className="relative w-[18rem] h-full max-sm:mt-4 flex flex-col items-center ">
-                <Image
-                  src="/Images/3rd.png"
-                  alt=""
-                  width={200}
-                  height={200}
-                  className="object-cover "
-                />
-                <div className="w-full min-h-24 rounded-lg bg-[#E87A42] ">
-                  <ul className="p-4">
-                    <li className="text-xl font-semibold text-center mb-2">
-                      Team <span className="capitalize">geronimo</span>
-                    </li>
-                    <li className="text-sm font-[500] capitalize ps-4">
-                      <span className="pe-3">1)</span>swadesh pal
-                    </li>
-                    <li className="text-sm font-[500] capitalize ps-4">
-                      <span className="pe-3">2)</span>swadesh pal
-                    </li>
-                    <li className="text-sm font-[500] capitalize ps-4">
-                      <span className="pe-3">3)</span>swadesh pal
-                    </li>
-                    <li className="text-sm font-[500] capitalize ps-4">
-                      <span className="pe-3">4)</span>swadesh pal
-                    </li>
-                    <li className="text-sm font-[500] capitalize ps-4">
-                      <span className="pe-3">5)</span>swadesh pal
-                    </li>
-                  </ul>
+              <div className="flex justify-between max-lg:items-center w-full min-h-[18rem] px-8 max-lg:flex-col">
+                <div className="relative w-[18rem] h-full  flex flex-col items-center max-sm:mt-4 ">
+                  <Image
+                    src="/Images/2nd.png"
+                    alt=""
+                    width={200}
+                    height={200}
+                    className="object-cover "
+                  />
+                  <div className="w-full min-h-24 rounded-lg bg-gray-300 ">
+                    <ul className="p-4">
+                      <li className="text-xl font-semibold text-center mb-2">
+                        Team{" "}
+                        <span className="capitalize">{list.winners[1]}</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="relative w-[18rem] h-full max-sm:mt-4 flex flex-col items-center ">
+                  <Image
+                    src="/Images/3rd.png"
+                    alt=""
+                    width={200}
+                    height={200}
+                    className="object-cover "
+                  />
+                  <div className="w-full min-h-24 rounded-lg bg-[#E87A42] ">
+                    <ul className="p-4">
+                      <li className="text-xl font-semibold text-center mb-2">
+                        Team{" "}
+                        <span className="capitalize">{list.winners[2]}</span>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
-          </div> */}
-          <div className="w-full h-[60vh] flex justify-center items-center">
-            <h1 className="text-2xl sm:text-4xl text-white font-griffy font-semibold capitalize">
-              stay tuned...........
-            </h1>
-          </div>
+          ) : (
+            <div className="w-full h-[60vh] flex justify-center items-center">
+              <h1 className="text-2xl sm:text-4xl text-white font-griffy font-semibold capitalize">
+                stay tuned...........
+              </h1>
+            </div>
+          )}
         </div>
       </div>
     </>
